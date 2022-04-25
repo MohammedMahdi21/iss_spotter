3dbeb004b0feb2f85aee335edb1ecc3cf28b95e2
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-04-25 23:41:45 +0000

    create a call back function fetchISSFlyOverTimes that takes coords as first argument and print out the values for error and data

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
 
@@ -18,4 +18,16 @@ fetchCoordsByIP('142.186.139.110', (error, coordinates) => {
   }
 
   console.log('It worked! Returned coordinates:' , coordinates);
+});
+
+const coords = { latitude: 45.451698303222656, longitude: -73.55460357666016 };
+
+fetchISSFlyOverTimes(coords, (error, body) =>{
+
+  if (error) {
+    console.log("It didn't work!" ,error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:',body);
 });
