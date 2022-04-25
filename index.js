b9a1fce2637e72e78feaf69069f27254c526ec08
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-04-25 22:32:28 +0000

    build a callback function and pass in our (IPv4) IP address string as the first argument to print out the values for error and data

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const {fetchMyIP} = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) =>{
+fetchMyIP((error, ip) => {
+
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:', ip);
+});
+
+fetchCoordsByIP('142.186.139.1110', (error, coordinates) => {
   
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned coordinates:' , coordinates);
 });
